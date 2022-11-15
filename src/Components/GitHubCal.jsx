3e7a9f0f15d8d3261ce4ab4f44083cb2190eafd6
Madhukesh K T <madhukeshthakur@gmail.com>
2022-11-15 16:05:23 +0530

    GitHub page updated

--- src/Components/GitHubCal.jsx
+++ src/Components/GitHubCal.jsx
@@ -1,41 +1,23 @@
-// import { Box, Center, Heading, Image } from '@chakra-ui/react'
-// import React from 'react'
-// import gitImage from "../Images/calender.JPG"
-
-// export default function GitHubCal() {
-//     return (
-//         <Box >
-//             <Center> <Heading fontSize="3rem" textDecoration="underline" color={'#B83280'} mt="1rem" mb="2rem">GitHub Calender</Heading></Center>
-//             <Box align={"center"} mt="0.5rem" >
-//                 <Image borderRadius={"1rem"} src={gitImage} alt="git-calender" />
-//             </Box>
-
-
-//         </Box>
-//     )
-// }
-
-// Steps to follow
-// npm i react-github-calendar
-// Add the below code snipet
 
 import React from 'react'
 import GitHubCalendar from "react-github-calendar"
 import { Box, Center, Heading } from '@chakra-ui/react'
 const GitHubCal = () => {
     return (
-        <Box mt={'3rem'}>
-            <Center> <Heading fontSize="3rem" textDecoration="underline" color={'#B83280'} mt="1rem" mb="2rem">GitHub Stats</Heading></Center>
-            <GitHubCalendar
-                username="madhukesh06"
-                blockSize={20}
-                blockMargin={5}
-                color="green"
-                fontSize={15}
-                m={40}
-                // w='70%'
-                style={{ margin: "auto", alignItems: "center" }}
-            />
+        <Box mt={'2rem'}>
+            <Center> <Heading fontSize="3rem" textDecoration="underline" color={'#B83280'} mb="2rem">GitHub Stats</Heading></Center>
+            <Box bg="#2D3748" color="white" m="auto" w={{ base: "95%", md: "80%" }} p={3} border="1px solid gray" borderRadius="0.5rem">
+
+                <GitHubCalendar
+                    username="madhukesh06"
+                    blockSize={20}
+                    blockMargin={5}
+                    color="green"
+                    fontSize={15}
+                    m="auto"
+                    style={{ margin: "auto", alignItems: "center" }}
+                />
+            </Box>
 
         </Box>
     )
